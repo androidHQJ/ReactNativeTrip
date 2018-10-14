3c7538a0f712eed38d86ab1677dbdf546db48ff4
heqijun <578391255@qq.com>
2018-10-14 21:05:07 +0800

    **组件基础知识学习**

--- HelloReactNative/App.js
+++ HelloReactNative/App.js
@@ -8,22 +8,24 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
+import HelloComponent from './components/HelloComponent';
+import LifeCycleComponent from './components/LifeCycleComponent';
 
-const instructions = Platform.select({
-    ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-    android:
-        '啦啊啦啦啦啦Double tap R on your keyboard to reload,\n' +
-        'Shake or press menu button for dev menu',
-});
+// const instructions = Platform.select({
+//     ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+//     android:
+//         '啦啊啦啦啦啦Double tap R on your keyboard to reload,\n' +
+//         'Shake or press menu button for dev menu',
+// });
 
 type Props = {};
 export default class App extends Component<Props> {
     render() {
         return (
             <View style={styles.container}>
-                <Text style={styles.welcome}>Hello HQJ!</Text>
-                <Text style={styles.instructions}>哈哈哈</Text>
-                <Text style={styles.instructions}>{instructions}</Text>
+                <LifeCycleComponent
+                    name="何其俊"
+                />
             </View>
         );
     }
@@ -32,19 +34,17 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#374cff',
+        backgroundColor: '#F5FCFF',
+        marginTop: 50,
     },
     welcome: {
         fontSize: 20,
         textAlign: 'center',
-        color:'#ff0000',
         margin: 10,
     },
     instructions: {
         textAlign: 'center',
-        color: '#ffffff',
+        color: '#333333',
         marginBottom: 5,
     },
 });
