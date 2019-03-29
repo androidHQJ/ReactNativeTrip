3e867a4e9a3048f169cba23e048dfabc3f5dcb68
heqijun <578391255@qq.com>
2019-03-29 20:04:06 +0800

    1.底部导航栏和navigator 基础用法  2.自定义顶部导航栏组件

---
+++ GitHub_APP/Boy.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Image,
+  Text,
+  View
+} from 'react-native';
+import Girl from './Girl.js'
+import NavigationBar from './NavigationBar.js'
+
+export default class Boy extends Component{
+    constructor(props){
+        super(props);
+        this.state={
+            word:''
+        }
+    }
+
+    render(){
+        return(
+            <View style={styles.container}>
+                <NavigationBar 
+                    statusBar={{
+                        backgroundColor:'red'
+                    }}
+                    style={{
+                        backgroundColor:'red'
+                    }}
+                    title={'Boy'}
+                />
+                <Text style={styles.text}>I am boy</Text>
+                <Text style = {styles.text} 
+                        onPress={()=>{
+                            this.props.navigator.push({
+                                component:Girl,
+                                params:{
+                                    word:'一支玫瑰',
+                                    onCallBack:(word)=>{
+                                        this.setState({
+                                            word:'收到女孩的：'+word
+                                        })
+                                    }
+                                }
+                            })
+                        }}> 送给女孩一支玫瑰 </Text>
+                < Text style = {
+                    styles.text
+                } >{this.state.word} </Text>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: 'white',
+    },
+    text:{
+        fontSize:20,
+    },
+  });
---
+++ GitHub_APP/typings/index.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="globals/node/index.d.ts" />
+/// <reference path="globals/vue/index.d.ts" />
+/// <reference path="modules/react-native/index.d.ts" />
+/// <reference path="modules/react/index.d.ts" />
